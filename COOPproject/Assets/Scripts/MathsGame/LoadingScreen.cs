d1d07dc0bd33593540e4df099a2fcfb0497163b9
s00175879 <s00175879@mail.itsligo.ie>
2018-04-09 22:46:30 +0100

    -created a script to allow player to post to facebook, still under development

--- COOPproject/Assets/Scripts/MathsGame/LoadingScreen.cs
+++ COOPproject/Assets/Scripts/MathsGame/LoadingScreen.cs
@@ -55,7 +55,8 @@ public class LoadingScreen : MonoBehaviour
 
         // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
         //AsyncOperation async = Application.LoadLevelAsync(scene);
-        AsyncOperation async = SceneManager.LoadSceneAsync("mathScene" + Random.Range(1, 3));
+        //there is 3 levels yet random range must be between 1 to 4 since, otherwise will never select the level 3
+        AsyncOperation async = SceneManager.LoadSceneAsync("mathScene" + Random.Range(1, 4));
 
 
         // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
---
+++ COOPproject/Assets/Scripts/facebookShare.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class facebookShare : MonoBehaviour {
+
+    private const string FACEBOOK_APP_ID = "161776944535663";
+    private const string FACEBOOK_URL = "http://www.facebook.com/dialog/feed";
+
+    void ShareToFacebook(string linkParameter, string nameParameter, string captionParameter, string descriptionParameter, string pictureParameter, string redirectParameter)
+    {
+        Application.OpenURL(FACEBOOK_URL + "?app_id=" + FACEBOOK_APP_ID +
+        "&link=" + WWW.EscapeURL(linkParameter) +
+        "&name=" + WWW.EscapeURL(nameParameter) +
+        "&caption=" + WWW.EscapeURL(captionParameter) +
+        "&description=" + WWW.EscapeURL(descriptionParameter) +
+        "&picture=" + WWW.EscapeURL(pictureParameter) +
+        "&redirect_uri=" + WWW.EscapeURL(redirectParameter));
+    }
+
+    //this function is the one attached to the facebook button in the game
+    public void submitToFacebookResults()
+    {
+        ShareToFacebook("","","","","","");
+        StartCoroutine(SceneLoadDelay());
+    }
+
+    IEnumerator SceneLoadDelay()
+    {
+        yield return new WaitForSeconds(3.5f);
+        SceneManager.LoadScene("titleScreen");
+    }
+}
